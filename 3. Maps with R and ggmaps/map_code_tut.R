b6b221baec8ed43c1945b8c869359f7a4ceb69e8
fed-ka <fedor.karmanov@gmail.com>
2017-05-05 13:09:40 -0400

    Second Upload

--- 3. Maps with R and ggmaps/map_code_tut.R
+++ 3. Maps with R and ggmaps/map_code_tut.R
@@ -2,21 +2,21 @@ library(rworldmap)
 
 #bring in the data
 met <- as.data.frame(read.csv("MetObjects_5k-sample.csv"))
-tate <- as.data.frame(read.csv("TateObjects_5k-sample.csv"))
 
 #country of origin frequency table
 countries.met <- as.data.frame(table(met$Country))
 colnames(countries.met) <- c("country", "value")
 
-nationality.met <- as.data.frame(table(met$Artist.Nationality))
-colnames(nationality.met) <- c("country", "value")
-
 #optional -- delete egypt b/c of skew
-countries.met <- countries.met[-21,]
 
 matched <- joinCountryData2Map(countries.met, joinCode="NAME", nameJoinColumn="country")
 
 mapCountryData(matched, nameColumnToPlot="value", mapTitle="Met Collection Country Sample", catMethod = "pretty", colourPalette = "heat")
 
-mapCountryData(matched, nameColumnToPlot="value", mapTitle="Eurasia",
-               mapRegion="Eurasia", colourPalette="terrain", catMethod="pretty")
+# without egypt
+countries.met.2 <- countries.met[-21,]
+matched.2 <- joinCountryData2Map(countries.met.2, joinCode="NAME", nameJoinColumn="country")
+
+mapCountryData(matched.2, nameColumnToPlot="value", mapTitle="Met Collection Country Sample", catMethod = "pretty", colourPalette = "heat")
+
+mapCountryData(matched, nameColumnToPlot="value", mapTitle="Eurasia",mapRegion="Eurasia", colourPalette="terrain", catMethod="pretty")
