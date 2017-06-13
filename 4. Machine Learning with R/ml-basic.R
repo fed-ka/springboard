e252b86a3a73277e9f77cbbf6274fd9d3ab5bd45
fed-ka <fedor.karmanov@gmail.com>
2017-06-13 00:59:49 -0400

    Updated again

--- 4. Machine Learning with R/ml-basic.R
+++ 4. Machine Learning with R/ml-basic.R
@@ -4,79 +4,50 @@ library("tm")
 library("dplyr")
 library("splitstackshape")
 
-# step 1. ingest your corpus training data
-corpus1 <- VCorpus(DirSource("Train", encoding = "UTF-8"), readerControl=list(language="English"))
-corpus1 <- tm_map(corpus1, content_transformer(stripWhitespace))
-corpus1 <- tm_map(corpus1, content_transformer(tolower))
-corpus1 <- tm_map(corpus1, content_transformer(removeNumbers))
-corpus1 <- tm_map(corpus1, removeWords, stopwords("English")) ## inspect your stopword lists
-corpus1 <- tm_map(corpus1, content_transformer(removePunctuation))
-corpus1 <- tm_map(corpus1, stemDocument, language = "english") # stem your words
+# Step 1. Ingest your training data and clean it.
+train <- VCorpus(DirSource("Training", encoding = "UTF-8"), readerControl=list(language="English"))
+train <- tm_map(train, content_transformer(stripWhitespace))
+train <- tm_map(train, content_transformer(tolower))
+train <- tm_map(train, content_transformer(removeNumbers))
+train <- tm_map(train, content_transformer(removePunctuation))
 
-corpus1.dtm<-DocumentTermMatrix(corpus1, control=list(wordLengths=c(1,Inf)))
-#corpus1.matrix<-as.matrix(corpus1.dtm, stringsAsFactors=F)
+# Step 2. Create your document term matrices for the training data.
+train.dtm <- as.matrix(DocumentTermMatrix(train, control=list(wordLengths=c(1,Inf))))
+#train.matrix <- as.matrix(train.dtm, stringsAsFactors=F)
 
-#create your document term matrix
-corpus2 <- VCorpus(DirSource("Predict", encoding = "UTF-8"), readerControl=list(language="English"))
+# Step 3. Repeat steps 1 & 2 above for the Test set.
+test <- VCorpus(DirSource("Test", encoding = "UTF-8"), readerControl=list(language="English"))
+test <- tm_map(test, content_transformer(stripWhitespace))
+test <- tm_map(test, content_transformer(tolower))
+test <- tm_map(test, content_transformer(removeNumbers))
+test <- tm_map(test, content_transformer(removePunctuation))
+test.dtm <- as.matrix(DocumentTermMatrix(test, control=list(wordLengths=c(1,Inf))))
 
-corpus2 <- tm_map(corpus2, content_transformer(stripWhitespace))
-corpus2 <- tm_map(corpus2, content_transformer(tolower))
-corpus2 <- tm_map(corpus2, content_transformer(removeNumbers))
-corpus2 <- tm_map(corpus2, removeWords, stopwords("English")) ## inspect your stopword lists
-corpus2 <- tm_map(corpus2, content_transformer(removePunctuation))
-corpus2 <- tm_map(corpus2, stemDocument, language = "english") # stem your words
+# Step 4. Make test and train matrices of identical length (find intersection)
+train.df <- data.frame(train.dtm[,intersect(colnames(train.dtm), colnames(test.dtm))])
+test.df <- data.frame(test.dtm[,intersect(colnames(test.dtm), colnames(train.dtm))])
 
-#create your document term matrix
-corpus2.dtm<-DocumentTermMatrix(corpus2, control=list(wordLengths=c(1,Inf)))
-#corpus2.matrix<-as.matrix(corpus2.dtm, stringsAsFactors=F)
-
-
-
-
-# convert to matrices for subsetting
-c1 <- as.matrix(corpus1.dtm, stringsAsFactors= FALSE) # training
-c2 <- as.matrix(corpus2.dtm, stringsAsFactors= FALSE) # testing
-
-c1.df <- data.frame(c1[,intersect(colnames(c1), colnames(c2))])
-c2.df <- data.frame(c2[,intersect(colnames(c2), colnames(c1))])
+# Step 5. Retrieve the correct labels for training data and put dummy values for testing data
+label.df <- data.frame(row.names(train.df))
+colnames(label.df) <- c("filenames")
+label.df<- cSplit(label.df, 'filenames', sep="_", type.convert=FALSE)
+train.df$corpus<- label.df$filenames_1
+test.df$corpus <- c("Neg")
 
 
-label.df<-data.frame(row.names(c1.df))
-colnames(label.df)<-c("filenames")
-label.df<-cSplit(label.df, 'filenames', sep="_", type.convert=FALSE)
-c1.df$corpus<- label.df$filenames
-c2.df$corpus <- c("Pos")
+# Step 5. Create folds of your data, then run the training once to inspect results
 
-
-#first create folds
-folds<-createFolds(c1.df$corpus, k=10) # k = number of folds (1-10) where 10 is best
-
-#use this section to run 1x and inspect your confusion matrix
-x<-folds$Fold01 #use this line of k = 10
-# x<-folds$Fold1 #use this line if k is less than 10; change to Fold2 for the second fold etc.
-df.train<- c1.df
-df.test<-c1.df
-df.model<-ksvm(corpus ~ ., data=df.train, kernel="rbfdot")
+df.train <- train.df
+df.test <- train.df
+df.model<-ksvm(corpus~., data= df.train, kernel="rbfdot")
 df.pred<-predict(df.model, df.test)
 con.matrix<-confusionMatrix(df.pred, df.test$corpus)
+print(con.matrix)
 
-con.matrix$byClass[[7]] #F1 score (combination of precision and recall)
-con.matrix$overall[[6]] #pvalue
-
-#run x-fold cross validation
-cv.results<-lapply(folds, function(x){
-  df.train<-df[-x,]
-  df.test<-df[x,]
-  df.model<-ksvm(corpus ~ ., data=df.train, kernel="rbfdot")
-  df.pred<-predict(df.model, df.test)
-  #con.matrix<-confusionMatrix(df.pred, df.test$corpus, positive = "Like")
-  #f1<-con.matrix$byClass[[7]]
-})
-unlist(cv.results) #this shows you the F1 score for each fold
-mean(unlist(cv.results)) #this is the average F1 score for all folds
-
-
-mat <- as.data.frame(df.pred)
-rownames(mat) <- rownames(c1.df)
-write.csv(mat, "results.csv")
+# Step 6. Run the final prediction on the test data and re-attach file names. 
+df.test <- test.df
+df.pred<-predict(df.model, df.test)
+results <- as.data.frame(df.pred)
+rownames(results) <- rownames(test.df)
+print(results)
 
